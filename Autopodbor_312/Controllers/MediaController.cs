5c4df0e29321c13edf3c999669c364f3957165a0
Roman <romakim32@gmail.com>
2022-11-11 14:25:24 +0600

    #2 Added main controllers

---
+++ Autopodbor_312/Controllers/MediaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Autopodbor_312.Controllers
+{
+    public class MediaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Autopodbor_312/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Autopodbor_312.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
