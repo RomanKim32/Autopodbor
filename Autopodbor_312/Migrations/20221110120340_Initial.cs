8a94b05a55ef44e1704018e90b881fe2234cfb84
Roman <romakim32@gmail.com>
2022-11-24 17:59:35 +0600

    Edited migrations

--- Autopodbor_312/Migrations/20221110120340_Initial.cs
+++ Autopodbor_312/Migrations/20221110120340_Initial.cs
@@ -4,7 +4,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Autopodbor_312.Migrations
 {
-    public partial class Initialize : Migration
+    public partial class Initial : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -50,62 +50,6 @@ namespace Autopodbor_312.Migrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "CarsBodyTypes",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    BodyType = table.Column<string>(nullable: true),
-                    Price = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_CarsBodyTypes", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "CarsBrands",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    Brand = table.Column<string>(nullable: true),
-                    Price = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_CarsBrands", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "CarsFuels",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    FuelsType = table.Column<string>(nullable: true),
-                    Price = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_CarsFuels", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "CarsYears",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    ManufacturesYear = table.Column<string>(nullable: true),
-                    Price = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_CarsYears", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
                 name: "News",
                 columns: table => new
                 {
@@ -113,7 +57,7 @@ namespace Autopodbor_312.Migrations
                         .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
                     Name = table.Column<string>(nullable: true),
                     Body = table.Column<string>(nullable: true),
-                    CreatedDate = table.Column<DateTime>(nullable: false)
+                    Photo = table.Column<string>(nullable: true)
                 },
                 constraints: table =>
                 {
@@ -128,7 +72,8 @@ namespace Autopodbor_312.Migrations
                         .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
                     Name = table.Column<string>(nullable: true),
                     Body = table.Column<string>(nullable: true),
-                    CreatedDate = table.Column<DateTime>(nullable: false)
+                    Photo = table.Column<string>(nullable: true),
+                    Video = table.Column<string>(nullable: true)
                 },
                 constraints: table =>
                 {
@@ -255,87 +200,6 @@ namespace Autopodbor_312.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
-            migrationBuilder.CreateTable(
-                name: "UploadFiles",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    Path = table.Column<string>(nullable: true),
-                    Type = table.Column<string>(nullable: true),
-                    NewsId = table.Column<int>(nullable: true),
-                    PortfolioId = table.Column<int>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_UploadFiles", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_UploadFiles_News_NewsId",
-                        column: x => x.NewsId,
-                        principalTable: "News",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
-                    table.ForeignKey(
-                        name: "FK_UploadFiles_Portfolio_PortfolioId",
-                        column: x => x.PortfolioId,
-                        principalTable: "Portfolio",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Orders",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    ServicesId = table.Column<int>(nullable: false),
-                    OrderTime = table.Column<DateTime>(nullable: false),
-                    UserName = table.Column<string>(nullable: false),
-                    PhoneNumber = table.Column<string>(nullable: false),
-                    Email = table.Column<string>(nullable: true),
-                    CarsBrandsId = table.Column<int>(nullable: false),
-                    CarsYearsId = table.Column<int>(nullable: false),
-                    CarsBodyTypesId = table.Column<int>(nullable: false),
-                    CarsFuelsId = table.Column<int>(nullable: false),
-                    Comment = table.Column<string>(nullable: true),
-                    Price = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Orders", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Orders_CarsBodyTypes_CarsBodyTypesId",
-                        column: x => x.CarsBodyTypesId,
-                        principalTable: "CarsBodyTypes",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Orders_CarsBrands_CarsBrandsId",
-                        column: x => x.CarsBrandsId,
-                        principalTable: "CarsBrands",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Orders_CarsFuels_CarsFuelsId",
-                        column: x => x.CarsFuelsId,
-                        principalTable: "CarsFuels",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Orders_CarsYears_CarsYearsId",
-                        column: x => x.CarsYearsId,
-                        principalTable: "CarsYears",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Orders_Services_ServicesId",
-                        column: x => x.ServicesId,
-                        principalTable: "Services",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
             migrationBuilder.CreateIndex(
                 name: "IX_AspNetRoleClaims_RoleId",
                 table: "AspNetRoleClaims",
@@ -372,41 +236,6 @@ namespace Autopodbor_312.Migrations
                 table: "AspNetUsers",
                 column: "NormalizedUserName",
                 unique: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Orders_CarsBodyTypesId",
-                table: "Orders",
-                column: "CarsBodyTypesId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Orders_CarsBrandsId",
-                table: "Orders",
-                column: "CarsBrandsId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Orders_CarsFuelsId",
-                table: "Orders",
-                column: "CarsFuelsId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Orders_CarsYearsId",
-                table: "Orders",
-                column: "CarsYearsId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Orders_ServicesId",
-                table: "Orders",
-                column: "ServicesId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_UploadFiles_NewsId",
-                table: "UploadFiles",
-                column: "NewsId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_UploadFiles_PortfolioId",
-                table: "UploadFiles",
-                column: "PortfolioId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -427,37 +256,19 @@ namespace Autopodbor_312.Migrations
                 name: "AspNetUserTokens");
 
             migrationBuilder.DropTable(
-                name: "Orders");
+                name: "News");
 
             migrationBuilder.DropTable(
-                name: "UploadFiles");
+                name: "Portfolio");
+
+            migrationBuilder.DropTable(
+                name: "Services");
 
             migrationBuilder.DropTable(
                 name: "AspNetRoles");
 
             migrationBuilder.DropTable(
                 name: "AspNetUsers");
-
-            migrationBuilder.DropTable(
-                name: "CarsBodyTypes");
-
-            migrationBuilder.DropTable(
-                name: "CarsBrands");
-
-            migrationBuilder.DropTable(
-                name: "CarsFuels");
-
-            migrationBuilder.DropTable(
-                name: "CarsYears");
-
-            migrationBuilder.DropTable(
-                name: "Services");
-
-            migrationBuilder.DropTable(
-                name: "News");
-
-            migrationBuilder.DropTable(
-                name: "Portfolio");
         }
     }
 }
