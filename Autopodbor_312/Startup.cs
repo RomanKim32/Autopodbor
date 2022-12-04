94ff4fec01eabff54c2d5c1b60c27f5cb932c187
LepesovAza <lepesov11@gmail.com>
2022-12-05 01:28:04 +0600

    #29 Created PartialView _Culture. Added Dropdown to select a language. Сreated Resources files for HomeController

--- Autopodbor_312/Startup.cs
+++ Autopodbor_312/Startup.cs
@@ -2,11 +2,15 @@ using Autopodbor_312.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace Autopodbor_312
 {
@@ -24,6 +28,20 @@ namespace Autopodbor_312
         {
             services.AddLocalization(opt => { opt.ResourcesPath = "Resources"; });
             services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix).AddDataAnnotationsLocalization();
+
+            services.Configure<RequestLocalizationOptions>(
+                opt =>
+                {
+                    var supportedCulteres = new List<CultureInfo>
+                    {
+                        new CultureInfo("ru"),
+                        new CultureInfo("kg")
+                    };
+                    opt.DefaultRequestCulture = new RequestCulture("ru");
+                    opt.SupportedCultures = supportedCulteres;
+                    opt.SupportedUICultures = supportedCulteres;
+                });
+
             services.AddControllersWithViews();
             string connection = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<AutopodborContext>(options => options.UseNpgsql(connection))
@@ -59,12 +77,14 @@ namespace Autopodbor_312
             app.UseAuthentication();
             app.UseAuthorization();
 
-            var supportedCultures = new[] {"ru", "kg"};
-            var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
-                .AddSupportedCultures(supportedCultures)
-                .AddSupportedUICultures(supportedCultures);
+            app.UseRequestLocalization(app.ApplicationServices.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
 
-            app.UseRequestLocalization(localizationOptions);
+            //var supportedCultures = new[] {"ru","kg"};
+            //var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
+            //    .AddSupportedCultures(supportedCultures)
+            //    .AddSupportedUICultures(supportedCultures);
+
+            //app.UseRequestLocalization(localizationOptions);
 
             app.UseEndpoints(endpoints =>
             {
