a53c7955bbb9bf3f2845c5b97f22b9838ff80263
LepesovAza <lepesov11@gmail.com>
2022-12-15 18:41:07 +0600

    #37 Created Controller for ContactInformation. Implemented functionality for Editing. Created Views for ContactInformation

---
+++ Autopodbor_312/Controllers/ContactInformationsController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Autopodbor_312.Models;
+
+namespace Autopodbor_312.Controllers
+{
+    public class ContactInformationsController : Controller
+    {
+        private readonly AutopodborContext _context;
+
+        public ContactInformationsController(AutopodborContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var contactInformation = _context.ContactInformation.First();
+            return View(contactInformation);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contactInformation = await _context.ContactInformation.FindAsync(id);
+            if (contactInformation == null)
+            {
+                return NotFound();
+            }
+            return View(contactInformation);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Email,PhoneNumber,LinkToInstagram,LinkToTiktok,LinkToYoutube,LinkToWhatsapp,LinkToTelegram")] ContactInformation contactInformation)
+        {
+            if (id != contactInformation.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contactInformation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContactInformationExists(contactInformation.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contactInformation);
+        }
+
+        private bool ContactInformationExists(int id)
+        {
+            return _context.ContactInformation.Any(e => e.Id == id);
+        }
+    }
+}
