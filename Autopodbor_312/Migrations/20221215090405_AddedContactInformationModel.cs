f57b9ef06c8e1aa920da81314680b06a5275ce7c
LepesovAza <lepesov11@gmail.com>
2022-12-15 15:16:03 +0600

    #37 Created Model for ContactInformation

---
+++ Autopodbor_312/Migrations/20221215090405_AddedContactInformationModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Autopodbor_312.Migrations
+{
+    public partial class AddedContactInformationModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContactInformation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    LinkToInstagram = table.Column<string>(nullable: true),
+                    LinkToTiktok = table.Column<string>(nullable: true),
+                    LinkToYoutube = table.Column<string>(nullable: true),
+                    LinkToWhatsapp = table.Column<string>(nullable: true),
+                    LinkToTelegram = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactInformation", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContactInformation");
+        }
+    }
+}
