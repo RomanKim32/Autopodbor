ceb327c3468c6ba1e021d6b1df5d6e5db5d9068a
Roman <romakim32@gmail.com>
2022-12-29 17:03:14 +0600

    #56 Edited Dbinitializer

--- Autopodbor_312/Controllers/ServiceController.cs
+++ Autopodbor_312/Controllers/ServiceController.cs
@@ -11,164 +11,162 @@ using System.Threading.Tasks;
 
 namespace Autopodbor_312.Controllers
 {
-	public class ServiceController : Controller
-	{
-		private readonly AutopodborContext _context;
-		private readonly IWebHostEnvironment _appEnvironment;
+    public class ServiceController : Controller
+    {
+        private readonly AutopodborContext _context;
+        private readonly IWebHostEnvironment _appEnvironment;
 
-		public ServiceController(AutopodborContext context, IWebHostEnvironment webHost)
-		{
-			_context = context;
-			_appEnvironment = webHost;
-		}
+        public ServiceController(AutopodborContext context, IWebHostEnvironment webHost)
+        {
+            _context = context;
+            _appEnvironment = webHost;
+        }
 
         [HttpGet]
-		public async Task<IActionResult> Services()
-		{
-			var sercices = await _context.Services.Where(s => s.NameRu != "Обратный звонок").Where(s => s.IsAdditional == false).OrderBy(s => s.Id).ToListAsync();
-			return View(sercices);
-		}
+        public async Task<IActionResult> Services()
+        {
+            var sercices = await _context.Services.Where(s => s.NameRu != "Обратный звонок").Where(s => s.IsAdditional == false).OrderBy(s => s.Id).ToListAsync();
+            return View(sercices);
+        }
 
-		[HttpGet]
+        [HttpGet]
         [Authorize(Roles = "admin")]
         public IActionResult CreateServices()
-		{
-			return View();
-		}
+        {
+            return View();
+        }
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		[Authorize(Roles = "admin")]
-		public async Task<IActionResult> CreateServices(Services service, IFormFile servicePhotoFile)
-		{
-			if (servicePhotoFile != null)
-			{
-				string filePath = Path.Combine(_appEnvironment.ContentRootPath, $"wwwroot/serviceImg/{servicePhotoFile.FileName}");
-				using (var fileStream = new FileStream(filePath, FileMode.Create))
-				{
-					await servicePhotoFile.CopyToAsync(fileStream);
-				}
-				service.Photo = $"/serviceImg/{servicePhotoFile.FileName}";
-			}
-			if (ModelState.IsValid)
-			{
-				service.IsAdditional = true;
-				_context.Add(service);
-				await _context.SaveChangesAsync();
-				if (service.IsAdditional == true)
-					return RedirectToAction("AdditionalServicesDetails", "Service");
-				return RedirectToAction("IndexServices", "Service");
-			};
-			return View(service);
-		}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> CreateServices(Services service, IFormFile servicePhotoFile)
+        {
+            if (ModelState.IsValid)
+            {
+                service.IsAdditional = true;
+                _context.Services.Add(service);
+                await _context.SaveChangesAsync();
+
+                string filePath = Path.Combine(_appEnvironment.ContentRootPath, $"wwwroot/serviceImg/Id={service.Id}&{servicePhotoFile.FileName}");
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await servicePhotoFile.CopyToAsync(fileStream);
+                }
+                service.Photo = $"/serviceImg/Id={service.Id}&{servicePhotoFile.FileName}";
+                await _context.SaveChangesAsync();
+                if (service.IsAdditional == true)
+                    return RedirectToAction("AdditionalServicesDetails", "Service");
+                return RedirectToAction("IndexServices", "Service");
+            };
+            return View(service);
+        }
 
 
-		[HttpGet]
+        [HttpGet]
         [Authorize(Roles = "admin")]
         public async Task<IActionResult> EditServices(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
-			var service = await _context.Services.FirstOrDefaultAsync(s => s.Id == id);
-			if (service == null)
-			{
-				return NotFound();
-			}
-			return View(service);
-		}
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var service = await _context.Services.FirstOrDefaultAsync(s => s.Id == id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+            return View(service);
+        }
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         [Authorize(Roles = "admin")]
         public async Task<IActionResult> EditServices(IFormFile servicePhotoFile, int id, [Bind("Id,NameRu,DescriptionRu,NameKy,DescriptionKy,IsAdditional,Photo")] Services service)
-		{
-			if (servicePhotoFile == null)
-			{
-				var currentService = await _context.Services.AsNoTracking().FirstOrDefaultAsync(s => s.Id == id);
-				service.Photo = currentService.Photo;
-			}
-			else
-			{
-				string filePath = Path.Combine(_appEnvironment.ContentRootPath, $"wwwroot/serviceImg/{servicePhotoFile.FileName}");
-				using (var fileStream = new FileStream(filePath, FileMode.Create))
-				{
-					await servicePhotoFile.CopyToAsync(fileStream);
-				}
-				service.Photo = $"/serviceImg/{servicePhotoFile.FileName}";
-			}
+        {
+            if (servicePhotoFile != null)
+            {
+                string oldFilePath = Path.Combine(_appEnvironment.ContentRootPath, $"wwwroot{service.Photo}");
+                if (System.IO.File.Exists(oldFilePath))
+                {
+                    System.IO.File.Delete(oldFilePath);
+                }
+                string filePath = Path.Combine(_appEnvironment.ContentRootPath, $"wwwroot/serviceImg/Id={service.Id}&{servicePhotoFile.FileName}");
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await servicePhotoFile.CopyToAsync(fileStream);
+                }
+                service.Photo = $"/serviceImg/Id={service.Id}&{servicePhotoFile.FileName}";
+            }
 
-			if (ModelState.IsValid)
-			{
-				try
-				{
-					_context.Update(service);
-					await _context.SaveChangesAsync();
-				}
-				catch (DbUpdateConcurrencyException)
-				{
-					if (!ServicesExists(service.Id))
-					{
-						return NotFound();
-					}
-					else
-					{
-						throw;
-					}
-				}
-				if (service.IsAdditional == true)
-					return RedirectToAction("AdditionalServicesDetails", "Service");
-				return RedirectToAction("Services", "Service");
-			}
-			return View(service);
-		}
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(service);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServicesExists(service.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                if (service.IsAdditional == true)
+                    return RedirectToAction("AdditionalServicesDetails", "Service");
+                return RedirectToAction("Services", "Service");
+            }
+            return View(service);
+        }
 
-		[HttpGet]
+        [HttpGet]
         [Authorize(Roles = "admin")]
         public async Task<IActionResult> DeleteServices(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-			var services = await _context.Services.FirstOrDefaultAsync(m => m.Id == id);
-			if (services == null)
-			{
-				return NotFound();
-			}
+            var services = await _context.Services.FirstOrDefaultAsync(m => m.Id == id);
+            if (services == null)
+            {
+                return NotFound();
+            }
 
-			return View(services);
-		}
+            return View(services);
+        }
 
-		[HttpPost, ActionName("DeleteServices")]
-		[ValidateAntiForgeryToken]
+        [HttpPost, ActionName("DeleteServices")]
+        [ValidateAntiForgeryToken]
         [Authorize(Roles = "admin")]
         public async Task<IActionResult> DeleteConfirmedServices(int id)
-		{
-			var services = await _context.Services.FindAsync(id);
-			// Нет проверки на то используется ли фото у другого сервиса
-    //        string filePath = Path.Combine(_appEnvironment.ContentRootPath, $"wwwroot{services.Photo}");
-    //        if (System.IO.File.Exists(filePath))
-    //        {
-				//System.IO.File.Delete(filePath);
-    //        }
+        {
+            var services = await _context.Services.FindAsync(id);
+            string filePath = Path.Combine(_appEnvironment.ContentRootPath, $"wwwroot{services.Photo}");
+            if (System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Delete(filePath);
+            }
             _context.Services.Remove(services);
-			await _context.SaveChangesAsync();
-			return RedirectToAction("AdditionalServicesDetails", "Service");
-		}
+            await _context.SaveChangesAsync();
+            return RedirectToAction("AdditionalServicesDetails", "Service");
+        }
 
-		[HttpGet]
+        [HttpGet]
         public async Task<IActionResult> AdditionalServicesDetails()
-		{
-			var additionalServicesList = await _context.Services.Where(s => s.IsAdditional == true).ToListAsync();
-			return View(additionalServicesList);
-		}
+        {
+            var additionalServicesList = await _context.Services.Where(s => s.IsAdditional == true).ToListAsync();
+            return View(additionalServicesList);
+        }
 
-		private bool ServicesExists(int id)
-		{
-			return _context.Services.Any(e => e.Id == id);
-		}
+        private bool ServicesExists(int id)
+        {
+            return _context.Services.Any(e => e.Id == id);
+        }
     }
 }
--- Autopodbor_312/DataSeeder/DbInitializer.cs
+++ Autopodbor_312/DataSeeder/DbInitializer.cs
@@ -13,7 +13,13 @@ namespace Autopodbor_312.DataSeeder
             {
                 var contactInformation = new ContactInformation()
                 {
-                    Email = "mgaldobin@mail.ru", LinkToInstagram = "https://instagram.com/autopodbor.312?igshid=YmMyMTA2M2Y=", LinkToTelegram = "", LinkToTiktok = "tiktok.com/@autopodbor.312", LinkToWhatsapp = "", LinkToYoutube = "", PhoneNumber = "0558303707"
+                    Email = "mgaldobin@mail.ru", 
+                    LinkToInstagram = "https://instagram.com/autopodbor.312?igshid=YmMyMTA2M2Y=", 
+                    LinkToTelegram = "", 
+                    LinkToTiktok = "tiktok.com/@autopodbor.312", 
+                    LinkToWhatsapp = "", 
+                    LinkToYoutube = "",
+                    PhoneNumber = "0558303707"
                 };
                 context.AddRange(contactInformation);
                 context.SaveChanges();
@@ -23,44 +29,48 @@ namespace Autopodbor_312.DataSeeder
             {
                 var services = new List<Services>
                 {
-                    new Services
-                    {
+                    new Services{
+                        Id = 1,
                         NameRu = "Подбор под ключ", DescriptionRu = "Пользуюясь подбором под ключ, вы заполняете анкету, указав в ней нужные данные по авто: марку, модель, бюджет и т.д. Мы, по вашим данным, ведём поиск автомобиля самостоятельно, выезжаем на все объявления и проверяем авто, до тех пор, пока не найдём стоящий вариант. Как нашли живое авто, вы встречаетесь с хозяином отдельно от нас и смотрите машину. Если вам всё нравится, мы проверяем авто на нескольких СТО и после этого торгуемся за машину и вы её покупаете. Оплата: Вначале оплачиваете 3000 сом - это предоплата, которая гарантирует нам, что за время поиска, вы не купите другое авто. Остальная сумма оплачивается после покупки авто.",
                         NameKy = "Название на кырг", DescriptionKy = "Описание на кырг",
-                        IsAdditional = false, Photo = "/serviceImg/tree-736885__480.jpg",
+                        IsAdditional = false, Photo = "/serviceImg/Id=1&tree-736885__480.jpg",
                     },
                     new Services {
+                        Id = 2,
                         NameRu = "Выездной осмотр", DescriptionRu = "Если вам понравился та или иная машина в объявлении, который вы хотите посмотреть, но у вас нет времени, либо нет навыков в авто, то вы обращаетесь к нам. Это можно сделать по звонку, в инстаграме, Вотсапе и телеграмме. Отправляете ссылку на объявление и все, ждете пока мы посмотрим машину. Мы сами звоним продавцу, встречаемся с ним, смотрим машину и отправляем вам все фото, видео, а также отчет о диагностике в том же чате. А вы после этого отправляете деньги на баланс телефона, либо на банковский счёт. Но если же вы хотите сами поприсутствовать на осмотре, то можете приехать и наблюдать за всем вживую. ",
 						NameKy = "Название на кырг", DescriptionKy = "Описание на кырг",
-						IsAdditional = false, Photo = "/serviceImg/tree-736885__480.jpg",
+						IsAdditional = false, Photo = "/serviceImg/Id=2&tree-736885__480.jpg",
                     },
                     new Services {
+                        Id = 3,
                         NameRu = "Дополнительные услуги", DescriptionRu = "Описание дополнительных услуг",
 						NameKy = "Название на кырг", DescriptionKy = "Описание на кырг",
-						IsAdditional = false, Photo = "/serviceImg/Add.jpg",
+						IsAdditional = false, Photo = "/serviceImg/Id=3&Add.jpg",
                     },
-                    new Services { NameRu = "Обратный звонок", DescriptionRu = "" ,NameKy = "", DescriptionKy = "", IsAdditional = false},
-                    new Services
-                    { 
+                    new Services {Id = 4, NameRu = "Обратный звонок", DescriptionRu = "" ,NameKy = "", DescriptionKy = "", IsAdditional = false},
+                    new Services{ 
+                        Id = 5,
                         NameRu = "Эксперт на день", DescriptionRu = "Вы нанимаете эксперта на определённое время и осматриваете авто, выбранные вами. Все перемещения производятся на машине эксперта. Услуга работает как по городу, так и на авторынке. Цена: от 6000 сом в день",
 						NameKy = "Название на кырг", DescriptionKy = "Описание на кырг",
-						IsAdditional = true, Photo = "/serviceImg/Add.jpg",
-					},
-                    new Services 
-                    {
-                        NameRu = "Обслуживание авто", DescriptionRu = "Хотите обслужить/отремонтировать свою машину, но боитесь, что попадёте на недобросовестный сервис? Для этого, обращаетесь к нам, объясняете задачу и отдаёте авто. Мы производим весь ремонт/обслуживание только в тех сервисах, в которых уверены. Все чеки работ, старые запчасти, фото/видео отчёты прилагаются. Цена: от 1000 сом и выше. (Зависит от масштаба работ)",
-						NameKy = "Название на кырг", DescriptionKy = "Описание на кырг",
-						IsAdditional = true, Photo = "/serviceImg/Add.jpg",
+						IsAdditional = true, Photo = "/serviceImg/Id=5&Add.jpg",
 					},
                     new Services {
+                        Id = 6,
+                        NameRu = "Обслуживание авто", DescriptionRu = "Хотите обслужить/отремонтировать свою машину, но боитесь, что попадёте на недобросовестный сервис? Для этого, обращаетесь к нам, объясняете задачу и отдаёте авто. Мы производим весь ремонт/обслуживание только в тех сервисах, в которых уверены. Все чеки работ, старые запчасти, фото/видео отчёты прилагаются. Цена: от 1000 сом и выше. (Зависит от масштаба работ)",
+						NameKy = "Название на кырг", DescriptionKy = "Описание на кырг",
+						IsAdditional = true, Photo = "/serviceImg/Id=6&Add.jpg",
+					},
+                    new Services {
+                        Id = 7,
                         NameRu = "Оценка стоимости авто перед продажей", DescriptionRu = "Если вы продаёте авто, но не знаете средних цен на вашу модель/марку и боитесь ошибиться в цене, то данная услуга для вас. Мы приезжаем и проверяем автомобиль, указываем на все недочёты и объясняем, за какую цену можно продать ваше авто в таком состоянии",
 						NameKy = "Название на кырг", DescriptionKy = "Описание на кырг",
-						IsAdditional = true, Photo = "/serviceImg/Add.jpg",
+						IsAdditional = true, Photo = "/serviceImg/Id=7&Add.jpg",
 					},
                     new Services { 
+                        Id = 8,
                         NameRu = "Сопровождение сделки", DescriptionRu = "Вы решили купить авто, но боитесь, что неправильно посчитаете деньги или вас обманут при оформлении документов, то можете воспользоваться этой услугой. Мы сопровождаем любую сделку (переоформление, оформление доверенности, подписание договора с компанией по привозу авто и т.д.) Цена: от 1000 сом и выше за сделку",
 						NameKy = "Название на кырг", DescriptionKy = "Описание на кырг",
-						IsAdditional = true, Photo = "/serviceImg/Add.jpg",
+						IsAdditional = true, Photo = "/serviceImg/Id=8&Add.jpg",
 					}
                 };
                 context.AddRange(services);
