307f830e99206c62627f946c7b6b764c52d9d3d9
Roman <romakim32@gmail.com>
2023-01-05 15:59:41 +0600

    #59 Added properties for news localization and portfolio

---
+++ Autopodbor_312/Migrations/20230105090055_AddedLocalizePropsInNewsAndPortfolio.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Autopodbor_312.Migrations
+{
+    public partial class AddedLocalizePropsInNewsAndPortfolio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Body",
+                table: "Portfolio");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Portfolio");
+
+            migrationBuilder.DropColumn(
+                name: "Body",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "News");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BodyKy",
+                table: "Portfolio",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BodyRu",
+                table: "Portfolio",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NameKy",
+                table: "Portfolio",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NameRu",
+                table: "Portfolio",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BodyKy",
+                table: "News",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BodyRu",
+                table: "News",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NameKy",
+                table: "News",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NameRu",
+                table: "News",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BodyKy",
+                table: "Portfolio");
+
+            migrationBuilder.DropColumn(
+                name: "BodyRu",
+                table: "Portfolio");
+
+            migrationBuilder.DropColumn(
+                name: "NameKy",
+                table: "Portfolio");
+
+            migrationBuilder.DropColumn(
+                name: "NameRu",
+                table: "Portfolio");
+
+            migrationBuilder.DropColumn(
+                name: "BodyKy",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "BodyRu",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "NameKy",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "NameRu",
+                table: "News");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Body",
+                table: "Portfolio",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Portfolio",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Body",
+                table: "News",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "News",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
--- Autopodbor_312/Models/Portfolio.cs
+++ Autopodbor_312/Models/Portfolio.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
 
 namespace Autopodbor_312.Models
 {
     public class Portfolio
     {
         public int Id { get; set; }
-        public string Name { get; set; }   
-        public string Body { get; set; }
+        public string NameRu { get; set; }   
+        public string BodyRu { get; set; }
+        public string NameKy { get; set; }
+        public string BodyKy { get; set; }
         public DateTime CreatedDate { get; set; }
         public bool Publicate { get; set; }
         public string MainImagePath { get; set; }
