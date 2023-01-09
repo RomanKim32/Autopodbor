4b0bcbfad8f39bad4358631562ed827d7b463e4d
Roman <romakim32@gmail.com>
2023-01-09 13:54:23 +0600

    #64 Edited Services Model

--- Autopodbor_312/Models/Services.cs
+++ Autopodbor_312/Models/Services.cs
@@ -6,14 +6,10 @@ namespace Autopodbor_312.Models
     public class Services
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "Поле обязательно к заполнению")]
         #nullable enable
 		public string? NameRu { get; set; }
-        [Required(ErrorMessage = "Поле обязательно к заполнению")]
         public string? DescriptionRu { get; set; }
-        [Required(ErrorMessage = "Поле обязательно к заполнению")]
         public string? NameKy { get; set; }
-        [Required(ErrorMessage = "Поле обязательно к заполнению")]
         public string? DescriptionKy { get; set; }
         public bool? IsAdditional { get; set; }
         public string? Photo { get; set; }
