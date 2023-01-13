c721e70ebdc2cd8c134c45b13017415f4f78fa55
Roman <romakim32@gmail.com>
2023-01-13 17:58:51 +0600

    #72 Moved the logic from the Orders controller to the repository.

---
+++ Autopodbor_312/Interfaces/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using Autopodbor_312.ViewModel;
+
+namespace Autopodbor_312.Interfaces
+{
+	public interface IOrderRepository
+	{
+		OrderViewModel CreateOrder(string serviceName);
+		void CreateOrder(string userName, string phoneNumber, string email, string comment, string carsBrandsId, string carsBodyTypesId, string carsYearsId, string carsFuelsId, string serviceId, string modelId);
+		void CreateCallBackAndAdditionalService(string userName, string phoneNumber, string email, string comment, string serviceName);
+	}
+}
