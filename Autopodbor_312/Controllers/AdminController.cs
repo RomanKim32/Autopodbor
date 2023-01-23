9383fcdc7e9760880ac4211b5ad69a8ff1763ceb
Roman <romakim32@gmail.com>
2023-01-23 16:18:42 +0600

    #100 Edited AdminController using

--- Autopodbor_312/Controllers/AdminController.cs
+++ Autopodbor_312/Controllers/AdminController.cs
@@ -2,7 +2,6 @@
 using Autopodbor_312.Models;
 using Autopodbor_312.ViewModel;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
